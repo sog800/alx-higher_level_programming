5fc0df8b5d25642b483d359817bf6c3e473ad3c6
Cyrus <daniellongwe800@gmail.com>
2023-01-03 12:19:44 -0800

    removeChar

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,9 +2,11 @@
 
 def remove_char_at(st, n):
     x = 0
+    rtn = ''
     for m in st:
         if x == n:
             x += 1
             continue
-        print(m, end='')
+        rtn += m
         x += 1
+    return rtn
