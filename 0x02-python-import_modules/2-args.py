ae3010a8c23da2752d5e67e2891a78dddeef7c50
Cyrus <daniellongwe800@gmail.com>
2023-01-05 04:58:19 -0800

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 
 def arg(args):
     num = len(args) - 1
-    print("{} arguments".format(num))
     if num > 0:
+        print("{} arguments:".format(num))
         x = 1
         while x <= num:
             print("{}: {}".format(x, args[x]))
             x += 1
-
+    else:
+        print("{} arguments.".format(num))
 
 if __name__ == "__main__":
     import sys
