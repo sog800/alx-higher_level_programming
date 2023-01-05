d51f8b4175077a4385b75d4466bbd6ef5f50cdfd
Cyrus <daniellongwe800@gmail.com>
2023-01-05 01:44:46 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,12 @@
 
 from add_0 import add
 
-a = 1
-b = 2
-sumn = add(a, b)
-print("{} + {} = {}".format(a, b, sumn))
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+if __name__ == '__main__':
+    main()
