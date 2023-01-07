fbada57ca667c68c2f2c5fc02fa496333ffe38b9
Cyrus <daniellongwe800@gmail.com>
2023-01-07 04:30:05 -0800

    index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    ln = len(my_list)
+    if idx < 0 or idx > ln:
+        return None
+    for i in my_list:
+        i = int(i)
+        if i == idx:
+            return my_list[i]
